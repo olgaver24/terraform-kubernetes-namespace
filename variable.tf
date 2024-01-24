9135246720ce964f0157692d872d192c1398cd53
olgaver24 <oleverevkina@gmail.com>
2024-01-24 17:54:11 -0600

    added module

---
+++ variable.tf
@@ -0,0 +1,47 @@
+variable namespace {
+    description = "Please, give a name"
+    type = string
+    default = ""
+}
+
+variable podlimit {
+    description = "Set limit for number of pods"
+    type = string
+    default = ""
+}
+
+variable pod_max_cpu_limit {
+  description = "Please set the pod size"
+  type = string
+  default = ""
+}
+
+variable pod_max_memory_limit {
+  description = "Please set the pod size"
+  type = string
+  default = ""
+}
+
+variable container_max_cpu_limit {
+  description = "Please set the container size"
+  type = string
+  default = ""
+}
+
+variable container_max_memory_limit {
+  description = "Please set the container size"
+  type = string
+  default = ""
+}
+
+variable annotations {
+  descriptions = "Please provide annotations"
+  type = map
+  default = {}
+}
+
+variable labels {
+  descriptions = "Please provide labels"
+  type = map
+  default = {}
+}
